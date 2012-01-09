121b583bb03d958de30103b54b77c2ea38a243b1
root <root@list.whyjustrun.ca>
2012-01-09 22:30:05 +0000

    A few deployment changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,8 @@ WhyJustRun::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  #config.assets.compile = false
+  config.assets.compile = true # NOTE-RWP Just for now
 
   # Generate digests for assets URLs
   config.assets.digest = true
