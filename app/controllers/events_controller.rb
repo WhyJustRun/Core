c6f7449e9b4e7bbace4931155e006b2a908d726a
Russell Porter <contact@russellporter.com>
2012-01-22 14:09:54 -0800

    Remove logging

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -122,9 +122,7 @@ class EventsController < ApplicationController
 		else 
 			raise ActionController::RoutingError.new('Not Found')
 		end
-		
-		logger.info results.inspect
-		
+				
 		event = Event.find(params[:id])
 		event.results_posted = 1
 		event.save
