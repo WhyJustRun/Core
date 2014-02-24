b4574ff918657bac02e1ac4e04e673690b1c4401
Russell Porter <contact@russellporter.com>
2014-02-24 17:05:49 +0000

    Even shorter way to find or create :)

--- app/controllers/live_results_controller.rb
+++ app/controllers/live_results_controller.rb
@@ -2,11 +2,7 @@ class LiveResultsController < ApplicationController
   before_filter :authenticate_user!, :only => :process_live_result_list
 
   def process_live_result_list
-    result = LiveResult.find_by event_id: params[:id]
-    if result.nil?
-      result = LiveResult.new
-    end
-    result.event_id = params[:id]
+    result = LiveResult.find_or_create_by event_id: params[:id]
     result.user_id = current_user.id
     result.data = request.body.read
     result.upload_time = Time.now
@@ -16,7 +12,7 @@ class LiveResultsController < ApplicationController
   def live_result_list
     result = LiveResult.find_by event_id: params[:id]
     respond_to do |format|
-      format.xml {render :xml => result.data}
+      format.xml { render :xml => result.data }
     end
   end
 end
