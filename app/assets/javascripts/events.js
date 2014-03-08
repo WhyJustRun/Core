8dd362b6cc3a00e9f2daa404f41c2d15959f28a4
Russell Porter <contact@russellporter.com>
2014-03-08 21:42:21 +0000

    Prefix club acronym on WJR calendar

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -2,7 +2,7 @@
 
 $(window).load(function() {
     $('#full-calendar').fullCalendar({
-        events: '/events.json',
+        events: '/events.json?prefix_club_acronym=true',
         timeFormat: 'h:mmtt',
         firstDay: 1,
     });
