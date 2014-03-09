6d7f1aa0c18e6aa9b61bb953d8719479c223490c
Russell Porter <contact@russellporter.com>
2014-03-09 00:17:08 +0000

    Don't create right away

--- app/controllers/live_results_controller.rb
+++ app/controllers/live_results_controller.rb
@@ -2,7 +2,7 @@ class LiveResultsController < ApplicationController
   before_filter :authenticate_user!, :only => :process_live_result_list
 
   def process_live_result_list
-    result = LiveResult.find_or_create_by event_id: params[:id]
+    result = LiveResult.find_or_initialize_by event_id: params[:id]
     result.user_id = current_user.id
     result.data = request.body.read
     result.upload_time = Time.now
