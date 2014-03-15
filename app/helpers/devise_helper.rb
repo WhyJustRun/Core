418a01c9405237f416c1f80d0b63ebd513f1264f
Russell Porter <contact@russellporter.com>
2014-03-14 23:31:23 -0700

    Fixes #24

---
+++ app/helpers/devise_helper.rb
@@ -0,0 +1,15 @@
+module DeviseHelper
+  def devise_error_messages!
+    return '' if resource.errors.empty?
+
+    messages = resource.errors.full_messages.map { |msg| content_tag(:p, content_tag(:strong,  msg)) }.join
+
+    html = <<-HTML
+    <div class="alert alert-danger">
+      #{messages}
+    </div>
+    HTML
+
+    html.html_safe
+  end
+end
