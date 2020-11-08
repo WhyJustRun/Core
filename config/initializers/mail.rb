0e034cdb0a373428fa710d320dc1accd1938f98c
Russell Porter <contact@russellporter.com>
2020-11-08 09:21:55 +0100

    Debug mailer issues

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -2,4 +2,6 @@ Rails.application.configure do
     config.action_mailer.smtp_settings = Settings.smtpSettings.to_hash
     config.action_mailer.delivery_method = :smtp
     config.action_mailer.default_url_options = { :host => Settings.host }
+    config.action_mailer.perform_deliveries = true
+    config.action_mailer.raise_delivery_errors = true
 end
