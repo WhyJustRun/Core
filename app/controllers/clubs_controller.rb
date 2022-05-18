ddb8b2d95bb12930de71c4403ae080ea1d3606d6
Russell Porter <contact@russellporter.com>
2022-05-18 08:44:56 +0200

    Remove club events api which seems unused

--- app/controllers/clubs_controller.rb
+++ app/controllers/clubs_controller.rb
@@ -16,20 +16,4 @@ class ClubsController < ApplicationController
   def map
     @clubs = Club.all_leaves.where(:visible => true)
   end
-
-  def events
-    @events = Event.limit(50).where("club_id = ?", params[:id]).order('date DESC')
-
-    respond_to do |wants|
-      wants.ics do
-        calendar = Icalendar::Calendar.new
-        calendar.x_wr_calname = Club.find(club_id).name
-        @events.each { |event|
-          calendar.add_event(event.to_ics)
-        }
-        calendar.publish
-        render :plain => calendar.to_ical
-      end
-    end
-  end
 end
