7c78c4f03f5091a04a8f1ee2a6126f734fcaa405
Russell Porter <contact@russellporter.com>
2024-09-10 22:06:46 +0200

    Configure allowed hosts

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,12 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
+
+  config.hosts = [
+    IPAddr.new("0.0.0.0/0"),        # All IPv4 addresses.
+    IPAddr.new("::/0"),             # All IPv6 addresses.
+    "localhost",                    # The localhost reserved domain.
+    "host.docker.internal:3000",
+    ENV["RAILS_DEVELOPMENT_HOSTS"]  # Additional comma-separated hosts for development.
+  ]
 end
